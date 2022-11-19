6da1a345f0e713b6c4fdfbd34f894b86ad7b0f7f
SauliusPadegimas <saulius.padegimas@gmail.com>
2022-11-19 15:58:06 +0200

    rooms

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,6 +1,8 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable prefer-regex-literals */
 const UserSchema = require('../schemas/userSchema');
 
+// funkcija validuoja registracijos formą, naudojamas su REST routeriu
 function regValidator(req, res, next) {
   const { username, password, password2 } = req.body;
   if (username.trim().length < 3) {
@@ -19,16 +21,32 @@ function regValidator(req, res, next) {
   return next();
 }
 
-async function userValidator(req, res, next) {
-  try {
-    const { secret } = req.body;
-    const user = await UserSchema.findOne({ secret });
-    if (!user) throw new Error();
-    next();
-  } catch (error) {
-    console.log('error on validator ===', error);
-    res.status(401).json({ error: true, message: 'login for posting', data: [] });
-  }
+// funkcija tikrina, ar duomenis siuncia registruotas vartotojas.
+// Naudojama su socket routais, todel grazina ne next(), o vartotoją arba false.
+async function userValidator(secret) {
+  const user = await UserSchema.findOne({ secret });
+  return user;
 }
 
-module.exports = { regValidator, userValidator };
+// funkcija tikrina, ar nera tusciu duomenu lauku.
+// Naudojama su socket routais, todel grazina ne next(), o tiesiog Boolean.
+async function addItemValidator(newItem) {
+  const { title, photo, bids, date } = newItem;
+  if (!!title.trim() && !!photo.trim() && !!bids && !!date) {
+    return true;
+  }
+  return false;
+}
+
+// tikrina ar nesibaigė likęs laikas
+function dateValidator(endDate) {
+  const endTime = new Date(endDate).getTime();
+  const timeNow = new Date().getTime();
+  const timeLeft = endTime - timeNow;
+  if (timeLeft <= 0) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = { regValidator, userValidator, addItemValidator, dateValidator };
--- src/routers/socketRouter.js
+++ src/routers/socketRouter.js
@@ -1,10 +1,35 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable brace-style */
+/* eslint-disable consistent-return */
 /* eslint-disable padded-blocks */
 /* eslint-disable object-curly-newline */
+const { userValidator, addItemValidator, dateValidator } = require('../middleware/validator');
 const ItemSchema = require('../schemas/itemSchema');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('addItem', async (newItem) => {
+    // &&& PRISIJUNGTI PRIE KAMBARIO
+    socket.on('join', (roomId) => {
+      socket.join(roomId);
+    });
+    // &&& ATSIJUNGTI NUO KAMBARIO
+    socket.on('leave', (roomId) => {
+      socket.leave(roomId);
+    });
+    // %%% PRIDĖTI PREKEI %%%
+    socket.on('addItem', async (newItem, secret) => {
+      // validuojam ar siuncia registruotas vartotojas
+      const ifValidUser = await userValidator(secret);
+      if (!ifValidUser) {
+        return socket.emit('errorOnAddItem', 'duomenis gali siųsti tik prisijungęs vartotojas');
+      }
+
+      // validuojam ar nėra tusčių laukų
+      const ifValidData = await addItemValidator(newItem);
+      if (!ifValidData) {
+        return socket.emit('errorOnAddItem', 'nepilnai užpildyti duomenys');
+      }
+
       const { title, photo, bids, date } = newItem;
       const item = new ItemSchema({
         title,
@@ -16,26 +41,67 @@ module.exports = (io) => {
         const resp = await item.save();
         console.log('resp from mangoose ===', resp);
         const allItems = await ItemSchema.find();
-        console.log('allItems ===', allItems);
+        socket.emit('errorOnAddItem', false);
         io.emit('items', allItems);
       } catch (error) {
         console.log('error ===', error);
       }
     });
 
-    socket.on('items', async () => {
-      const allItems = await ItemSchema.find();
-      console.log('allItems ===', allItems);
-      socket.emit('items', allItems);
+    socket.on('items', async (data) => {
+      // gauti vieną prekę
+      if (data) {
+        const item = await ItemSchema.findById(data);
+        socket.emit('oneItem', item);
+      }
+
+      // gauti visas prekes
+      else {
+        const allItems = await ItemSchema.find();
+        console.log('allItems ===', allItems);
+        socket.emit('items', allItems);
+      }
     });
 
-    //   socket.on('join', ({ sender, receiver }) => {
-    //     let room = [sender, receiver];
-    //     room = room.sort().join('&');
-    //     socket.join(room);
-    //     console.log(`${sender} have joined room: ${room}`);
-    //     const usersMessages = messages.filter((x) => x.room === room);
-    //     io.in(room).emit('message', { usersMessages, room });
-    //   });
+    // %%% PRIDĖTI STATYMĄ %%%
+    socket.on('update', async (data) => {
+      const { secret, id, price } = data;
+      // validuojam ar siuncia registruotas vartotojas
+      const ifValidUser = await userValidator(secret);
+      if (!ifValidUser) {
+        return socket.emit('errorOnUpdate', 'aukcione dalyvauti gali tik prisijungęs vartotojas');
+      }
+      const item = await ItemSchema.findById(id);
+      const prevPrice = item.bids[0].price;
+
+      if (price <= prevPrice) {
+        return socket.emit('errorOnUpdate', 'galite siūlyti tik aukštesnę, negu esama kaina');
+      }
+
+      // validuojam ar nesibaigė statymų laikas
+      const ifOnTime = dateValidator(item.date);
+      if (!ifOnTime) {
+        return socket.emit('errorOnUpdate', 'pasibaigė laikas');
+      }
+
+      const bidsArr = item.bids;
+      const newBid = {
+        user: ifValidUser.username,
+        price,
+      };
+      bidsArr.unshift(newBid);
+
+      const updatedItem = await ItemSchema.findByIdAndUpdate(
+        id,
+        { bids: bidsArr },
+        {
+          returnOriginal: false,
+        }
+      );
+      socket.emit('errorOnUpdate', false);
+      io.to(id).emit('oneItem', updatedItem);
+      const allItems = await ItemSchema.find();
+      socket.emit('items', allItems);
+    });
   });
 };
